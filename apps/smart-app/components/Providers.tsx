0f46f3c273683008606d58a9b7da7574a7e5dc2f
ThanushKrishna <jeevanthanush@gmail.com>
2023-11-16 12:01:05 +0530

    testcases for db connection

--- apps/smart-app/components/Providers.tsx
+++ apps/smart-app/components/Providers.tsx
@@ -3,11 +3,17 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client
 
 import React from 'react'
 
+const graphqlhost="localhost"
+
 const Providers = ({ children } : { children: React.ReactNode}) => {
+  
     const client = new ApolloClient({
-        uri: 'http://localhost:3000/api/graphql/',
+        uri: `http://${graphqlhost}:3000/api/graphql/`,
         cache: new InMemoryCache(),
       });
+
+      console.log(`http://${graphqlhost}:3000/api/graphql/`);
+
   return (
 
     <ApolloProvider client={client}>
--- apps/smart-app/graphql/resolvers.ts
+++ apps/smart-app/graphql/resolvers.ts
@@ -36,16 +36,30 @@ export const dateScalar = new GraphQLScalarType({
   },
 });
 
+console.log("DB Connection is going or not")
+
 export const resolvers = {
   
   Date: dateScalar,
 
   Query: {
     app_user: async (parent: any, args: any, context: Context) => {
-      return await context.prisma.app_user.findMany();
+      try{
+        return await context.prisma.app_user.findMany();
+      }
+      catch(err){
+          console.log(err);
+      }
+      
     },
     user_data: async (parent: any, args: any, context: Context) => {
-      return await context.prisma.user_data.findMany();
+      
+      try{
+        return await context.prisma.user_data.findMany();
+      }
+      catch(err){
+          console.log(err);
+      }
     },
 
   },
@@ -108,58 +122,63 @@ export const resolvers = {
     },
 
     createUserData: async (parent: any, args: any, context: Context) => {
-      console.log("this is CreateUserData block");            
-      return await context.prisma.user_data.create({
-        data: {
-          Vehicle_No: args.input.Vehicle_No,   
-          data_owner_id:args.input.data_owner_id,                 
-          RC_No: args.input.RC_No,
-          Registered_Date: args.input.Registered_Date,          	
-          Owner: args.input.Owner,                 
-          Owner_dob: args.input.Owner_dob,    
-          Ownership_type: args.input.Ownership_type,                  
-          Vehicle_type: args.input.Vehicle_type,          
-          Year_of_manufacuring: args.input.Year_of_manufacuring,  
-          GVW: args.input.GVW,                   
-          Chasis_No: args.input.Chasis_No,             
-          Engine_No: args.input.Engine_No,  
-          FC_due_Date: args.input.FC_due_Date,
-          tax_due_Date: args.input.tax_due_Date,
-          Vehicle_color: args.input.Vehicle_color,         
-          Vehice_norms: args.input.Vehice_norms,                     
-          CC: args.input.CC,                    
-          Make: args.input.Make,                  
-          Model: args.input.Model,                 
-          Insurance_provider: args.input.Insurance_provider,    
-          Insurance_dueDate: args.input.Insurance_dueDate,  
-          Policy_No: args.input.Policy_No,             
-          Permit_No: args.input.Permit_No,             
-          Permit_category: args.input.Permit_category,       
-          Mobile_No1: args.input.Mobile_No1,            
-          Mobile_No2: args.input.Mobile_No2,            
-          Email_id: args.input.Email_id,              
-          Adhar_No: args.input.Adhar_No,              
-          Adhar_doc: args.input.Adhar_doc,             
-          PanCard_No: args.input.PanCard_No,            
-          Pan_doc: args.input.Pan_doc,               
-          Nominee: args.input.Nominee,               
-          Nominee_dob: args.input.Nominee_dob,  
-          Emission_dueDate: args.input.Emission_dueDate,              
-          Fuel_type: args.input.Fuel_type,             
-          Hypothecation_bank: args.input.Hypothecation_bank,    
-          Hypothecation_city: args.input.Hypothecation_city,    
-          RTO: args.input.RTO,                   
-          Referred_by: args.input.Referred_by,           
-          Comments: args.input.Comments,              
-          Customer_type: args.input.Customer_type,         
-          Martial_status: args.input.Martial_status,        
-          TP_Insurance_provider: args.input.TP_Insurance_provider,  
-          TP_dueDate: args.input.TP_dueDate,          
-          GST_No: args.input.GST_No,                
-          Insurance_type: args.input.Insurance_type,
-        },       
+      console.log("this is CreateUserData block");    
+      try{        
+        return await context.prisma.user_data.create({
+          data: {
+            Vehicle_No: args.input.Vehicle_No,   
+            data_owner_id:args.input.data_owner_id,                 
+            RC_No: args.input.RC_No,
+            Registered_Date: args.input.Registered_Date,          	
+            Owner: args.input.Owner,                 
+            Owner_dob: args.input.Owner_dob,    
+            Ownership_type: args.input.Ownership_type,                  
+            Vehicle_type: args.input.Vehicle_type,          
+            Year_of_manufacuring: args.input.Year_of_manufacuring,  
+            GVW: args.input.GVW,                   
+            Chasis_No: args.input.Chasis_No,             
+            Engine_No: args.input.Engine_No,  
+            FC_due_Date: args.input.FC_due_Date,
+            tax_due_Date: args.input.tax_due_Date,
+            Vehicle_color: args.input.Vehicle_color,         
+            Vehice_norms: args.input.Vehice_norms,                     
+            CC: args.input.CC,                    
+            Make: args.input.Make,                  
+            Model: args.input.Model,                 
+            Insurance_provider: args.input.Insurance_provider,    
+            Insurance_dueDate: args.input.Insurance_dueDate,  
+            Policy_No: args.input.Policy_No,             
+            Permit_No: args.input.Permit_No,             
+            Permit_category: args.input.Permit_category,       
+            Mobile_No1: args.input.Mobile_No1,            
+            Mobile_No2: args.input.Mobile_No2,            
+            Email_id: args.input.Email_id,              
+            Adhar_No: args.input.Adhar_No,              
+            Adhar_doc: args.input.Adhar_doc,             
+            PanCard_No: args.input.PanCard_No,            
+            Pan_doc: args.input.Pan_doc,               
+            Nominee: args.input.Nominee,               
+            Nominee_dob: args.input.Nominee_dob,  
+            Emission_dueDate: args.input.Emission_dueDate,              
+            Fuel_type: args.input.Fuel_type,             
+            Hypothecation_bank: args.input.Hypothecation_bank,    
+            Hypothecation_city: args.input.Hypothecation_city,    
+            RTO: args.input.RTO,                   
+            Referred_by: args.input.Referred_by,           
+            Comments: args.input.Comments,              
+            Customer_type: args.input.Customer_type,         
+            Martial_status: args.input.Martial_status,        
+            TP_Insurance_provider: args.input.TP_Insurance_provider,  
+            TP_dueDate: args.input.TP_dueDate,          
+            GST_No: args.input.GST_No,                
+            Insurance_type: args.input.Insurance_type,
+          },       
       })
-    },
+    }
+    catch(err){
+      console.log(err);
+    }
+  },
     
     testaddclient: async (parent: any, args: any, context: Context) => {
       console.log("this is testCreateUserdata block");        
