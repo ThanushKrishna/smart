adc7139d91648fa160b22d189d1c0d40323c66ee
ThanushKrishna <jeevanthanush@gmail.com>
2024-01-30 00:55:45 +0530

    cors added for the domain

--- apps/smart-app/pages/api/graphql.ts
+++ apps/smart-app/pages/api/graphql.ts
@@ -1,16 +1,53 @@
-import { ApolloServer } from '@apollo/server';
-import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { ApolloServer } from 'apollo-server-micro';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 import  prisma  from "../../prisma/db";
 import { typeDefs } from '@/graphql/schema'
 import { resolvers } from '@/graphql/resolvers'
+import Cors from 'micro-cors'
+
+let serverStarted = false;
 
 export type Context = {
   prisma: PrismaClient;
 }
 
-const apolloServer = new ApolloServer<Context>({ typeDefs, resolvers });
+console.log("process.env.NEXT_PUBLIC_ORIGIN", process.env.NEXT_PUBLIC_ORIGIN);
 
-export default startServerAndCreateNextHandler(apolloServer, {
-	context: async (req, res) => ({ req, res, prisma }),
+const cors = Cors({
+  allowMethods: ['POST', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  origin: process.env.NEXT_PUBLIC_ORIGIN, // replace with your origins
+});
+
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req, res }: { req: NextApiRequest; res: NextApiResponse }) => ({ req, res, prisma }),
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+let handler:any;
+
+(async () => {
+  if (!serverStarted) {
+    await apolloServer.start();
+    serverStarted = true;
+    handler = apolloServer.createHandler({ path: '/api/graphql' });
+  }
+})();
+
+export default cors((req, res) => {
+  if (serverStarted) {
+    return handler(req, res);
+  }
+  // If the server is not started yet, end the response and return
+  res.end();
+  return;
 });
